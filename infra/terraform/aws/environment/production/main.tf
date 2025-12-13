468c665db082d290434a864c4b4e9ae2635e6ef1
Federica Iola Caselli <160833909+fedecas123@users.noreply.github.com>
2025-12-13 13:10:19 +0100

    fix: critical deployment fix changes

--- infra/terraform/aws/environment/production/main.tf
+++ infra/terraform/aws/environment/production/main.tf
@@ -9,7 +9,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "ai-ticket-platform-remote-state-bucket-zxmluk37"
+    bucket  = "ai-ticket-remote-state-bucket-2efbqdyz"
     key     = "remote-state/production/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
